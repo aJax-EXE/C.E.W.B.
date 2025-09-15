a3dbf5ae8a1008e0b2aee75af42544f1aa4a40d8
AJax_Exe <alyssajackson436@gmail.com>
2025-09-14 18:35:30 -0600

    Arduino Encoder Works, but acts funny?

---
+++ TestMotorWEncoder/sketch_sep10a/sketch_sep10a.ino
@@ -0,0 +1,19 @@
+// Libraries for the code needed
+//#include <SparkFun_TB6612.h>  // https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_Library/tree/master
+#include <ESP32Encoder.h>     // https://github.com/madhephaestus/ESP32Encoder.git
+
+#define CLK 13 // CLK ENCODER 
+#define DT 18 // DT ENCODER 
+ 
+ESP32Encoder encoder;
+ 
+void setup () { 
+  encoder.attachHalfQuad(DT, CLK);
+  encoder.setCount(0);
+  Serial.begin(115200);
+}
+ 
+void loop () {    
+  long newPosition = encoder.getCount() / 2;
+  Serial.println(newPosition);
+} 

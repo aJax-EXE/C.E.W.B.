e83d49caf6b15a60db81c8ff3a9677a69ede21de
AJax_Exe <alyssajackson436@gmail.com>
2025-11-10 09:37:19 -0700

    More Testing Needed

--- InteractionToCCode/MoreTest.cpp
+++ InteractionToCCode/MoreTest.cpp
@@ -19,9 +19,33 @@ int main() {
     fcntl(fd, F_SETFL);
     fd = open("/dev/ttyACM0", O_RDWR);
     char buffer[1024];
+
+    
+
+    char word[100];  // Buffer to store the word (up to 99 chars + '\0')
+
+    
    
     while(true) {
+
+        FILE *file = fopen("./data.txt", "r");  // Open file for reading
+        if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+        }
         mgSize = read(fd, buffer, 1);
+
+        
+
+        if (fscanf(file, "%99s", word) == 1) {  // Read one word from the file
+
+        printf("The word in the file is: %s\n", word);
+         } else {
+        printf("No word found or read error.\n");
+        }
+        fclose(file);  // Close the file
+
+
 
         if(buffer[0] != delim) {
             inData += buffer[0];

78d208d4f6c62dd7c8f7c1364ff1ffe81c050597
AJax_Exe <alyssajackson436@gmail.com>
2025-11-11 10:53:03 -0700

    MotorRunningwEncoderCounter

--- ArduinoEncoderWOLibraries/ArduinoEncoderWOLibraries.ino
+++ ArduinoEncoderWOLibraries/ArduinoEncoderWOLibraries.ino
@@ -3,19 +3,25 @@
 #define DT 5
 
 // Motor Drive PMW Pins
-// #define
-// #de
+#define MotorIn1 9
+#define MotorIn2 10
 
 int counter = 0;
 int currentStateCLK;
 int lastStateCLK;
 String currentDir = "";
 
+int PWMVal = 0;
+
 void setup() {
 
   // Set encoder pins as inputs
   pinMode(CLK, INPUT);
   pinMode(DT, INPUT);
+
+  // Setting the Motor Pins
+  pinMode(MotorIn1, OUTPUT);
+  pinMode(MotorIn2, OUTPUT);
 
   // Setup Serial Monitor
   Serial.begin(9600);
@@ -29,6 +35,83 @@ void loop() {
   // Read the current state of CLK
   currentStateCLK = digitalRead(CLK);
 
+  // Ramp Up Forward
+  digitalWrite(MotorIn1, LOW);
+  for (PWMVal; PWMVal <= 255; PWMVal++) {
+    currentStateCLK = digitalRead(CLK);
+    analogWrite(MotorIn2, PWMVal);
+    positionCheck();
+    delay(10);
+  }
+
+  // Full Forward Speed for one second
+  // Figure out how to pause the process for this time while keeping 
+  // The counter from the encoder going
+  delay(1000);
+
+  // Ramp Down Forward
+  for (PWMVal; PWMVal >= 0; PWMVal--) {
+    currentStateCLK = digitalRead(CLK);
+    analogWrite(MotorIn2, PWMVal);
+    positionCheck();
+    delay(10);
+  }
+
+  // Ramp Up Backward
+  digitalWrite(MotorIn2, LOW);
+  for (PWMVal = 0; PWMVal <= 255; PWMVal++) {
+    currentStateCLK = digitalRead(CLK);
+    analogWrite(MotorIn1, PWMVal);
+    positionCheck();
+    delay(10);
+  }
+
+  // Full Backwards Speed for one second
+  // Figure out how to pause the process for this time while keeping 
+  // The counter from the encoder going
+  delay(1000);
+
+  // Ramp Down Forward
+  for (PWMVal = 255; PWMVal >= 0; PWMVal--) {
+    currentStateCLK = digitalRead(CLK);
+    analogWrite(MotorIn1, PWMVal);
+    positionCheck();
+    delay(10);
+  }
+
+  // // If last and current state of CLK are different, then pulse occurred
+  // // React to only 1 state change to avoid double count
+  // if (currentStateCLK != lastStateCLK && currentStateCLK == 1) {
+
+  //   // If the DT state is different than the CLK state then
+  //   // the encoder is rotating CCW so decrement
+  //   if (digitalRead(DT) != currentStateCLK) {
+  //     counter--;
+  //     currentDir = "CCW";
+  //   } else {
+  //     // Encoder is rotating CW so increment
+  //     counter++;
+  //     currentDir = "CW";
+  //   }
+
+  //   Serial.print("Direction: ");
+  //   Serial.print(currentDir);
+  //   Serial.print(" | Counter: ");
+  //   Serial.println(counter);
+  // } //else {
+  // //   Serial.print("Direction: ");
+  // //   Serial.print(currentDir);
+  // //   Serial.print(" | Counter: ");
+  // //   Serial.println(counter);
+  // // }
+
+  // // Remember last CLK state
+  // lastStateCLK = currentStateCLK;
+  // // counter++;
+
+}
+
+void positionCheck () {
   // If last and current state of CLK are different, then pulse occurred
   // React to only 1 state change to avoid double count
   if (currentStateCLK != lastStateCLK && currentStateCLK == 1) {
@@ -48,12 +131,12 @@ void loop() {
     Serial.print(currentDir);
     Serial.print(" | Counter: ");
     Serial.println(counter);
-  } else {
-    Serial.print("Direction: ");
-    Serial.print(currentDir);
-    Serial.print(" | Counter: ");
-    Serial.println(counter);
-  }
+  } //else {
+  //   Serial.print("Direction: ");
+  //   Serial.print(currentDir);
+  //   Serial.print(" | Counter: ");
+  //   Serial.println(counter);
+  // }
 
   // Remember last CLK state
   lastStateCLK = currentStateCLK;
--- InteractionToCCode/MoreTest.cpp
+++ InteractionToCCode/MoreTest.cpp
@@ -12,34 +12,38 @@ using namespace std;
 const char delim = '|';
 
 int main() {
-    int fd;
+    // int fd;
     ssize_t mgSize = 0;
     string inData;
 
-    fcntl(fd, F_SETFL);
-    fd = open("/dev/ttyACM0", O_RDWR);
-    char buffer[1024];
+    // fcntl(fd, F_SETFL);
+    // fd = open("/dev/ttyACM0", O_RDWR);
+    // char buffer[1024];
+    string buffer;
+    fstream fs;
+    fs.open("/dev/ttyACM0", ios_base::in|ios_base::out|ios_base::binary);
 
     
 
     char word[100];  // Buffer to store the word (up to 99 chars + '\0')
 
-    
    
     while(true) {
 
         FILE *file = fopen("./data.txt", "r");  // Open file for reading
         if (file == NULL) {
-        perror("Error opening file");
-        return 1;
+            perror("Error opening file");
+            return 1;
         }
-        mgSize = read(fd, buffer, 1);
-
+        // mgSize = read(fd, buffer, 1);
+        getline(fs, buffer);
+        // if(buffer == "") continue;
         
 
         if (fscanf(file, "%99s", word) == 1) {  // Read one word from the file
 
         printf("The word in the file is: %s\n", word);
+        rewind(file);
          } else {
         printf("No word found or read error.\n");
         }
@@ -47,12 +51,17 @@ int main() {
 
 
 
-        if(buffer[0] != delim) {
-            inData += buffer[0];
-        } else {
-            cerr << "Hit Delimiter" << endl;
-             cout << "DEBUG: " << inData << endl;
-        }
+        // while(buffer[0] != delim)
+        // {
+        //     inData += buffer[0];
+        //     cout << "DEBUG: " << inData;
+        // }
+        // if(buffer[0] != delim) {
+            
+        // } else {
+        cerr << "Hit Delimiter" << endl;
+        cout << "DEBUG: " << buffer << endl;
+        // }
        
 
         // memset(buffer, 0, mgSize);
@@ -60,6 +69,8 @@ int main() {
 
     }
    
-    close(fd);
+    
+    fs.close();
+    // close(fd);
     return 0;
 }

b1952fec8732fe6b0b8e5b9f96ad817111fa03ff
AJax_Exe <alyssajackson436@gmail.com>
2026-01-30 17:02:54 -0700

    Starting the parsing; finished with formatting

--- Formatting/FormattingWChars/FormattingWChars.ino
+++ Formatting/FormattingWChars/FormattingWChars.ino
@@ -23,17 +23,25 @@ const int bufferSize = 50;
 
 // The complete information format that needs to be dissected (and the temp info variable)
 char info[bufferSize] = " ";
-char tempInfo[16] = " ";
-// String info, tempInfo;
+// char tempInfo[16] = " ";
 
 // Check if all of the values are formatted correctly or can be formatted correctly
 bool correctFormat, done = false;
 
-// Binary counter for ease of function access and char insertion
-bool count = 0b000;
+// Binary counter to determine what values are incorrect and need to be fixed (And with the different types of error values that can exist)
+uint8_t errorVal = 0b000000;
+enum {
+  INFO_TYPE_ERROR = 0b100000,
+  VOLT_ERROR = 0b010000,
+  ANGLE_TYPE_ERROR = 0b001000,
+  KP_ERROR = 0b000100,
+  KI_ERROR = 0b000010,
+  KD_ERROR = 0b000001
+} ;
 
-bool formatCheck(char, float, char, float, float, float, float);
-void transferValue(float, char*, bool&);
+
+bool formatCheck(char, float, char, float, float, float, float, byte&);
+void transferValues(char, float, char, float, float, float, float, char*, int);
 
 void setup() {
   // put your setup code here, to run once:
@@ -42,82 +50,96 @@ void setup() {
 
 void loop() {
   // 'done' variable makes sure the code only runs once
-  while (done == false) {
+  while (!done) {
     // Checking the formatting and values of the input values to be compared
-    correctFormat = formatCheck(infoType, voltage, angleType, angle, Kp, Ki, Kd);
+    correctFormat = formatCheck(infoType, voltage, angleType, Kp, Ki, Kd, errorVal);
     
-    if (correctFormat == false) {
+    if (!correctFormat) {
       // Checks failed somewhere...
-      Serial.println("Sorry user, but one or more variables inputted for proper formatting are incorrect. Please review and try again.");
-      done = true;
-    } else if (correctFormat == true) {
-      // Check succeeds - Adding the voltage info
-      info[0] = infoType;
-      info[strlen(info)] = 'V';
-      transferValue(voltage, tempInfo);
+      Serial.println("Sorry user, but one or more of the following variables inputted for proper formatting are incorrect: ");
 
-      strcat(info, tempInfo);
-      tempInfo[0] = '\0';
-
-      // Adding the angle info
-      info[strlen(info)] = 'A';
-      info[strlen(info)] = angleType;
-      transferValue(angle, tempInfo);
-
-      strcat(info, tempInfo);
-      tempInfo[0] = '\0';
+      // Check each bit and print corresponding message
+      if (errorVal & INFO_TYPE_ERROR) {
+        Serial.println("* Incorrect information type character given");
+      }
+      if (errorVal & VOLT_ERROR) {
+        Serial.println("* Incompatiable voltage value given");
+      }
+      if (errorVal & ANGLE_TYPE_ERROR) {
+        Serial.println("* Incorrect angle type given");
+      }
+      if (errorVal & KP_ERROR) {
+        Serial.println("* Incompatiable Kp value given");
+      }
+      if (errorVal & KI_ERROR) {
+        Serial.println("* Incompatiable Ki value given");
+      }
+      if (errorVal & KD_ERROR) {
+        Serial.println("* Incompatiable Kd value given");
+      }
       
-      // Adding the PID controller info
-      strcat(info, ";P");
-      transferValue(Kp, tempInfo);
-
-      strcat(info, tempInfo);
-      tempInfo[0] = '\0';
-
-      info[strlen(info)] = 'I';
-      transferValue(Ki, tempInfo);
-
-      strcat(info, tempInfo);
-      tempInfo[0] = '\0';
-
-      info[strlen(info)] = 'D';
-      transferValue(Kd, tempInfo);
-
-      strcat(info, tempInfo);
-      info[strlen(info)] = '!';
-      Serial.println(info);
-
-      // // The overall length of the formatted information array should be 49 characters long
-      Serial.println(strlen(info));
+      Serial.println("Please review and try again.");
       done = true;
     }
+    else {
+      // Check succeeds
+      transferValues(infoType, voltage, angleType, angle, Kp, Ki, Kd, info, bufferSize);
+
+      // The overall length of the formatted information array should be 49 characters long (48 info characters + '\0' (null character))
+      Serial.print("Formatted Info: ");
+      Serial.println(info);
+      Serial.print("Number of info characters: ");
+      Serial.println(strlen(info));
+      Serial.print("Number of total characters: ");
+      Serial.println(strlen(info) + 1);
+      done = true;
+    }
+
+    // done == true
   }
 }
 
 // Function that checks if the given values are valid and can be formatted correctly
-bool formatCheck(char infoType, float voltage, char angleType, float angle, float Kp, float Ki, float Kd) {
+bool formatCheck(char infoType, float voltage, char angleType, float Kp, float Ki, float Kd, byte& error) {
   if (infoType != '$' && infoType != '#') {
-    return false;
-  } else if (voltage/100 >= 1) { 
-    return false; 
-  } else if (angleType != 'D' && angleType != 'R') {
-    return false;
-  } else if (Kp/1000 >= 1 && Ki/1000 >= 1 && Kd/1000 >= 1){
-    return false;
-  } else { return true; }
+    error |= 0b100000;
+  }
+  if (voltage/100 >= 1) { 
+    error |= 0b010000;
+  }
+  if (angleType != 'D' && angleType != 'R') {
+    error |= 0b001000;
+  }
+  if (Kp/1000 >= 1 || Ki/1000 >= 1 || Kd/1000 >= 1){
+    if (Kp/1000 >= 1) {
+      error |= 0b000100;
+    } 
+    if (Ki/1000 >= 1) {
+      error |= 0b000010;
+    }
+    if (Kd/1000 >= 1) {
+      error |= 0b000001;
+    }
+  }
+
+  return error == 0;
 }
 
 // Function that turns the float values into there proper form of XXX.XXXX into a char array
-void transferValue(float value, char* temp, bool& count) {
-  int whole = (int)value;
-  int frac  = round((value - whole) * 10000);
+void transferValues(char infoType, float voltage, char angleType, float angle, float Kp, float Ki, float Kd, char* info, int bufferSize) {
+  // Variables for the whole number and decimal points of each value
+  int wholeVolt = (int)voltage;
+  int wholeAng = (int)angle;
+  int wholeKp = (int)Kp;
+  int wholeKi = (int)Ki;
+  int wholeKd = (int)Kd;
 
-  switch (count) {
-    case 1:
-    
-  }
-  if (count == 0) {
-    snprintf(temp, 16, "%03d.%04d", whole, frac);
-  }
-  
+  int fracVolt  = round((voltage - wholeVolt) * 10000);
+  int fracAng  = round((angle - wholeAng) * 10000);
+  int fracKp  = round((Kp - wholeKp) * 10000);
+  int fracKi  = round((Ki - wholeKi) * 10000);
+  int fracKd  = round((Kd - wholeKd) * 10000);
+
+  // Combining all of the variables together into the necessary format
+  snprintf(info, bufferSize, "%cV%02d.%04dA%c%03d.%04d;P%03d.%04dI%03d.%04dD%03d.%04d!", infoType, wholeVolt, fracVolt, angleType, wholeAng, fracAng, wholeKp, fracKp, wholeKi, fracKi, wholeKd, fracKd);
 } 
---
+++ Formatting/ParsingWChars/ParsingWChars.ino
@@ -0,0 +1,116 @@
+// Setting the variables that the format will carry
+// USES CHAR ARRAYS
+float voltage, angle, Kp, Ki, Kd;
+String infoLabel, angleType;
+
+// The complete information format that needs to be dissected (and the temp info variable)
+String info, tempInfo;
+int startIndex;
+
+// This is an example of the format that is used: $V12.3220AD044.8745;P009.0000I000.1000D003.5000!
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Starting the loop only when data is received
+  while (Serial.available() > 0) {
+    // Getting the message and cutting off the end character
+    info = Serial.readStringUntil('!');
+
+    // Initial String size check
+    if (info.length() != 47) {
+      Serial.println("Sorry this data is not formatted correctly. Please try again.");
+      break;
+    }
+
+    // Determining the label (Setting or Feedback)
+    if (info.charAt(0) == '$') {
+      infoLabel = "Master Info";
+    } else if (info.charAt(0) == '#') {
+      infoLabel = "Feedback Info";
+    }
+
+    // Remove the first division label character
+    info.remove(0,1);
+
+    // Getting the different bits of information - voltage
+    startIndex = info.indexOf('V');
+    // Serial.println(startIndex);
+
+    tempInfo = info.substring(startIndex+1, startIndex+8);
+    voltage = tempInfo.toFloat();
+    // Serial.println(voltage, 4);
+
+
+    // Getting the different bits of information - angle
+    startIndex = info.indexOf('A');
+    // Serial.println(startIndex);
+
+    tempInfo = info.substring(startIndex+2, startIndex+10);
+    // Determining the label (Degrees or Radians)
+    if (info.charAt(startIndex+1) == 'D') {
+      angleType = "Degrees";
+    } else if (info.charAt(0) == 'R') {
+      angleType = "Radians";
+    }
+    angle = tempInfo.toFloat();
+    Serial.print("Label: ");
+    // Serial.println(angleType);
+    // Serial.println(angle, 4);
+
+    
+    // Getting the proportional constant
+    startIndex = info.indexOf('P');
+    // Serial.println(startIndex);
+
+    tempInfo = info.substring(startIndex+1, startIndex+9);
+    Kp = tempInfo.toFloat();
+    // Serial.println(Kp, 4);
+
+
+    // Getting the integral constant
+    startIndex = info.indexOf('I');
+    // Serial.println(startIndex);
+
+    tempInfo = info.substring(startIndex+1, startIndex+9);
+    Ki = tempInfo.toFloat();
+    // Serial.println(Ki, 4);
+
+
+    // Getting the derivative constant
+    startIndex = info.lastIndexOf('D');
+    // Serial.println(startIndex);
+
+    tempInfo = info.substring(startIndex+1, startIndex+9);
+    Kd = tempInfo.toFloat();
+    // Serial.println(Kd, 4);
+
+     //Printing all of the information
+  Serial.print("Entered Data: ");
+  Serial.println(info);
+  Serial.print("Label: ");
+  Serial.println(infoLabel);
+
+  Serial.print("Voltage: ");
+  Serial.println(voltage, 4);
+
+  Serial.print("Angle Type: ");
+  Serial.println(angleType);
+
+  Serial.print("Angle: ");
+  Serial.println(angle, 4);
+
+  Serial.print("Kp: ");
+  Serial.println(Kp, 4);
+
+  Serial.print("Ki: ");
+  Serial.println(Ki, 4);
+
+  Serial.print("Kd: ");
+  Serial.println(Kd, 4);
+  }
+}
+
